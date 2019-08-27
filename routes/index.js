45937c3195acbb6a1e1b9bccbd0e5e316381fa7d
Alejandro Rivera <ariverat@tiempodev.com>
2019-08-27 12:10:51 -0500

    Fix metadata retrival

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,20 @@ const url = 'http://169.254.169.254/latest/meta-data';
 /**
  * @param { Promise } p
  */
-const handle = p => p.then(r => [r]).catch(e => [undefined, e]);
+const handle = p => p.then(r => [r.text()]).catch(e => [undefined, e]);
 
 /* GET home page. */
 router.get('/', async function (_, res) {
-  const [instance_id, error] = await handle(fetch(`${url}/instance-id`));
-  console.log(error);
-  console.log(instance_id);
-  res.render('index', { instance_id: 'i-0f4a807825d1b6d42', az: 'us-west-1' });
+  const failedToFetch = 'Failed to fetch';
+
+  const [instance_id, instanceIdError] = await handle(fetch(`${url}/instance-id`));
+  const [az, azError] = await handle(fetch(`${url}/placement/availability-zone`));
+
+  if (instanceIdError || azError) {
+    return res.render('index', { instance_id: failedToFetch, az: failedToFetch });
+  }
+
+  res.render('index', { instance_id, az });
 });
 
 module.exports = router;
