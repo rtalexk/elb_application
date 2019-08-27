8f6774e6ce79cd2badef465244e38402fc33b093
Alejandro Rivera <ariverat@tiempodev.com>
2019-08-27 11:50:58 -0500

    Design Instance Card info

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (_, res) {
+  res.render('index', { instance_id: 'i-0f4a807825d1b6d42', az: 'us-west-1' });
 });
 
 module.exports = router;
