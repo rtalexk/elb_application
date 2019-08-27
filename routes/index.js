80e36bd3bd954c790f95ddb0c60008e2a7008648
Alejandro Rivera <ariverat@tiempodev.com>
2019-08-27 11:51:15 -0500

    Retrieve instance metadata

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+// Instance local link URL
+const url = 'http://169.254.169.254/latest/meta-data';
+
+/**
+ * @param { Promise } p
+ */
+const handle = p => p.then(r => [r]).catch(e => [undefined, e]);
 
 /* GET home page. */
 router.get('/', async function (_, res) {
+  const [instance_id, error] = await handle(fetch(`${url}/instance-id`));
+  console.log(error);
+  console.log(instance_id);
   res.render('index', { instance_id: 'i-0f4a807825d1b6d42', az: 'us-west-1' });
 });
 
