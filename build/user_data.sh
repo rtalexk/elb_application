428ca3a4fc6589c476f78b0d5f5d88012b21a236
Alejandro Rivera <ariverat@tiempodev.com>
2019-08-28 12:46:10 -0500

    Refactor bootstrap script

---
+++ build/user_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download code
+wget https://github.com/rtalexk/elb_classic/archive/master.zip
+unzip master.zip
+rm master.zip
+
+# Grant exec permissions and execute
+chmod 755 elb_classic-master/build/bootstrap.sh
+./elb_classic-master/build/bootstrap.sh
